ef86d5745fee70227466432488901a2990590b7e
Asha <satpathyasharani@gmail.com>
2023-07-15 12:28:49 +0530

    started the project

---
+++ scrip.js
@@ -0,0 +1,86 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+
+let currentPlayer;
+let gameGrid;
+
+//array
+const winningPositions = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+];
+
+//let's create a function to initialise the game
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];//hide value
+    //UI pr empty bhi karna padega boxes ko
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        //one more thing is missing, initialise box with css properties again
+        box.classList = `box box${index+1}`;
+    });
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+initGame();
+
+
+function swapTurn() {
+    if(currentPlayer === "X") {
+        currentPlayer = "O";
+    }
+    else {
+        currentPlayer = "X";
+    }
+    //UI Update
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+
+function checkGameOver() {
+ 
+}
+
+
+function handleClick(index) {
+    if(gameGrid[index] === "" ) {
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none";
+        //swap karo turn ko
+        swapTurn();
+        //check koi jeet toh nahi gya
+        checkGameOver();
+    }
+}
+
+function handleClick(index) {
+    if(gameGrid[index] === "" ) {
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none";
+        //swap karo turn ko
+        swapTurn();
+        //check koi jeet toh nahi gya
+        checkGameOver();
+    }
+}
+
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        handleClick(index);
+    })
+});
+
+newGameBtn.addEventListener("click", initGame);
